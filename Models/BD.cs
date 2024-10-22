7fa46e3d34a60f5651f95fed85da37dc1450542b
EITAN MAS <48240669@ort>
2024-10-22 09:00:05 -0300

    Continuacion

---
+++ Models/BD.cs
@@ -0,0 +1,6 @@
+using System.Data.SqlClient;
+using Dapper;
+static class BD
+{
+private static string _connectionString = @"Server=LocalHost; DataBase=TP9 - Login usando JS; Trusted_Connection=True;";
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,9 @@
+public class Usuario
+{
+public int IdUsername{get;set;}
+public string UserName{get;set;}
+public string Contraseña{get;set;}
+public string Nombre{get;set;}
+public string Apellido{get;set;}
+public string Telefono{get;set;}
+}
