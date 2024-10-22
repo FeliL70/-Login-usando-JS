e28614522cc2cbe7a15fb0b077362f9ee4a5ac0e
EITAN MAS <48240669@ort>
2024-10-22 08:26:09 -0300

    nuevo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP9___Login_usando_JS.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
